741d46ec4bf63753bcb327e42af589ad79853b2d
Shankar1056 <dayashankargupta86@gmail.com>
2018-01-27 01:36:19 +0530

    first commit .

---
+++ app/src/main/java/com/apextechies/apexschool/MainActivity.java
@@ -0,0 +1,23 @@
+package com.apextechies.apexschool;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+}
