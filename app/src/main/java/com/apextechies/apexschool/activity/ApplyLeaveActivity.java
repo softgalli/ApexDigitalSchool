ee7a1a1d2bc06c8e375237f15faccbbbe29132b8
Pradeep <pradeepkumar.s@vvdntech.in>
2018-02-01 14:47:16 +0530

    Some Changes are done for Attendance Module

---
+++ app/src/main/java/com/apextechies/apexschool/activity/ApplyLeaveActivity.java
@@ -0,0 +1,19 @@
+package com.apextechies.apexschool.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.apextechies.apexschool.R;
+
+/**
+ * Created by Welcome on 2/1/2018.
+ */
+
+public class ApplyLeaveActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apply_leave_activity);
+    }
+}
--- app/src/main/java/com/apextechies/apexschool/activity/MainActivity.java
+++ app/src/main/java/com/apextechies/apexschool/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.apextechies.apexschool.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,22 +11,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.apextechies.apexschool.R;
 import com.crashlytics.android.Crashlytics;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.applyLeave)
+    TextView applyLeave;
+    @BindView(R.id.leaveRequestList)
+    TextView leaveRequestList;
+    @BindView(R.id.approveLeave)
+    TextView approveLeave;
+    @BindView(R.id.takeAttendance)
+    TextView takeAttendance;
+    @BindView(R.id.myAttendance)
+    TextView myAttendance;
+    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mActivity = this;
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -70,5 +91,22 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @OnClick({R.id.applyLeave, R.id.leaveRequestList, R.id.approveLeave, R.id.takeAttendance, R.id.myAttendance})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.applyLeave:
+                startActivity(new Intent(mActivity, ApplyLeaveActivity.class));
+                break;
+            case R.id.leaveRequestList:
+                break;
+            case R.id.approveLeave:
+                break;
+            case R.id.takeAttendance:
+                break;
+            case R.id.myAttendance:
+                break;
+        }
     }
 }
